9ebb069af483ef04f4f65dd12f9cdbe9f498fb15
trandatdt <trandat021197@gmail.com>
2018-04-08 18:05:00 +0700

    Optimized code

--- main.py
+++ main.py
@@ -1,64 +1,77 @@
 import random
-import math
+from math import factorial, exp
 import time
 
-N_QUEENS = 8
+N_QUEENS = 100
+TEMPERATURE = 4000
 
 
-# Create N x N chess board
-def create_board(N_QUEENS):
-    queens = []
-    for i in range(N_QUEENS):
-        queens.append(random.randint(1, N_QUEENS))
-    return queens
+def create_board(n):
+    chess_board = []
+    temp = list(range(n))
+    random.shuffle(temp)
+    while len(temp) > 0:
+        queen = random.choice(temp)
+        chess_board.append(queen)
+        temp.remove(queen)
+    del temp
+    return chess_board
 
 
-# Cost function
-def cost(queens):
-    h = 0
-    for i in range(len(queens)):
-        for j in range(i + 1, len(queens)):
-            # Queens are in the same row
-            if queens[i] == queens[j]:
-                h += 1
-            offset = j - i
-            # Queens are in the same diagonal
-            if queens[i] == queens[j] - offset \
-               or queens[i] == queens[j] + offset:
-                h += 1
-    return h
+def cost(chess_board):
+    threat = 0
+
+    m_chessboard = [chess_board.index(i) - i for i in chess_board]
+    for i in set(m_chessboard):
+        count_i = m_chessboard.count(i)
+        if count_i > 1:
+            temp = factorial(count_i) / (factorial(2) * factorial(count_i - 2))
+            threat += int(temp)
+    del m_chessboard
+
+    a_chessboard = [chess_board.index(i) + i for i in chess_board]
+    for i in set(a_chessboard):
+        count_i = a_chessboard.count(i)
+        if count_i > 1:
+            temp = factorial(count_i) / (factorial(2) * factorial(count_i - 2))
+            threat += int(temp)
+    del a_chessboard
+
+    return threat
 
 
 def simulated_annealing():
     solution_found = False
     answer = create_board(N_QUEENS)
-    temperature = 4000
+    t = TEMPERATURE
+    sch = 0.99
 
-    while temperature > 0:
-        temperature *= 0.99
-        successor_queen = answer[:]
-        successor_queen[random.randint(
-            0, N_QUEENS - 1)] = random.randint(1, N_QUEENS)
-        delta = cost(successor_queen) - cost(answer)
-
+    while t > 0:
+        t *= sch
+        successor = answer[:]
+        while True:
+            index_1 = random.randrange(0, N_QUEENS - 1)
+            index_2 = random.randrange(0, N_QUEENS - 1)
+            if index_1 != index_2:
+                break
+        successor[index_1], successor[index_2] = successor[index_2], successor[index_1]
+        delta = cost(successor) - cost(answer)
         if delta < 0:
-            answer = successor_queen[:]
+            answer = successor[:]
         else:
-            p = math.exp(-delta / temperature)
+            p = exp(-delta / t)
             if random.uniform(0, 1) < p:
-                answer = successor_queen[:]
-
+                answer = successor[:]
         if cost(answer) == 0:
             solution_found = True
-            print_chessboard(answer)
+            print_chess_board(answer)
             break
-
     if solution_found is False:
-        print("Failed", answer)
+        print("Failed")
 
 
-def print_chessboard(answer):
-    for row in list(enumerate(answer, start=1)):
+def print_chess_board(board):
+    for row in list(enumerate(board)):
         print(row)
 
 
